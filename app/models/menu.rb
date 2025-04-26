33fca11ae3719b852ccba667659e08607fc83b74
Evio Junior <evio.junior@spezi.com.br>
2025-04-26 19:48:05 +0000

    Added has_many association to menu

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,5 +2,7 @@ class Menu < ApplicationRecord
   enum :availability, { all_time: 0, seasonal: 1 }, default: :all_time
   enum :day_period, { all_day: 0, dinner: 1, lunch: 2, breakfast: 3 }, default: :all_day
 
+  has_many :menu_items
+
   validates_presence_of :availability, :day_period, :name
 end
