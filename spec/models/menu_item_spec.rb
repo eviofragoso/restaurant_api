b93e851955a9fa3b521cf980ea7313e9232bd631
Evio Junior <evio.junior@spezi.com.br>
2025-04-27 02:39:12 +0000

    Added tests to cover validations behaviour

--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe MenuItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validation" do
+    context "presence validation" do
+      it "requires the presence of name" do
+        expect(FactoryBot.build(:menu_item, name: nil)).not_to be_valid
+      end
+
+      it "requires the presence of description" do
+        expect(FactoryBot.build(:menu_item, description: nil)).not_to be_valid
+      end
+
+      it "requires the presence of menu" do
+        expect(FactoryBot.build(:menu_item, menu_id: nil)).not_to be_valid
+      end
+    end
+  end
 end
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Menu, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validation" do
+    context "presence validation" do
+      it "requires the presence of name" do
+        expect(FactoryBot.build(:menu, name: nil)).not_to be_valid
+      end
+
+      it "requires the presence of day_period" do
+        expect(FactoryBot.build(:menu, day_period: nil)).not_to be_valid
+      end
+
+      it "requires the presence of availability" do
+        expect(FactoryBot.build(:menu, availability: nil)).not_to be_valid
+      end
+    end
+  end
 end
